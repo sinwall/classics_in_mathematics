889d2eeb795b161c288ac189fa4664eab005e2e3
Byungchang So <sinwall@snu.ac.kr>
2024-02-29 14:34:00 +0900

    module THREE imported internally

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 
 app.use('/static', express.static(path.join(__dirname, 'static')));
+app.use('/module-three', express.static(path.join(__dirname, 'node_modules/three')));
+// app.use('/module-three', express.static(path.join(__dirname, 'node_modules/three/src/Three.js')));
+// app.use('/module-three-addons', express.static(path.join(__dirname, 'node_modules/three/examples/jsm')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
