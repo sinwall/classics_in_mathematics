725514a029177fc71fe9592595b7d8e3d9e6cbe0
Byungchang So <sinwall@snu.ac.kr>
2024-02-29 14:39:17 +0900

    get origin by function call; toward capsulizing Vector3 api

--- static/construction.js
+++ static/construction.js
@@ -1,9 +1,99 @@
-// import {Vector3} from "https://unpkg.com/three@0.160.0/src/Three.js"
 import {Vector3} from 'three'
+// import {Vector3} from '../node_modules/three'
 
-let origin = new Vector3();
+let _origin = new Vector3();
 let e_x = new Vector3(1);
 let e_y = new Vector3(0, 1);
 let e_z = new Vector3(0, 0, 1);
 
+function origin() {
+    return _origin.clone();
+}
+
+class Vector {
+    constructor(x=0, y=0, z=0) {
+        this._x = x;
+        this._y = y;
+        this._z = z;
+        this._v3 = new Vector3(this._x, this._y, this._z);
+    }
+    add(vec) {
+        return this.addCoords(vec.x, vec.y, vec.z);
+    }
+    addCoords(dx=0, dy=0, dz=0) {
+        let result = this.clone();
+        result.x += dx;
+        result.y += dy;
+        result.z += dz;
+        return result;
+    }
+    clone() {
+        return Vector(this.x, this.y, this.z);
+    }
+    dilate(t) {
+        let result = this.clone();
+        result.x *= t;
+        result.y *= t;
+        result.z *= t;
+        return result;
+    }
+    distTo(vec) {
+        let dist = 0;
+        dist += (this.x - vec.x) ** 2;
+        dist += (this.y - vec.y) ** 2;
+        dist += (this.z - vec.z) ** 2;
+        dist = Math.sqrt(dist);
+        return dist;
+    }
+    dot(vec) {
+        let result = 0;
+        result += this.x*vec.x;
+        result += this.y*vec.y;
+        result += this.z*vec.z;
+        return result;
+    }
+    get x() {
+        return this._x;
+    }
+    get y() {
+        return this._y;
+    }
+    get z() {
+        return this._z;
+    }
+    norm () {
+        let result = 0;
+        result += this.x ** 2;
+        result += this.y ** 2;
+        result += this.z ** 2;
+        result = Math.sqrt(result);
+        return result;
+    }
+    set x(value) {
+        this._x = value;
+    }
+    set y(value) {
+        this._y = value;
+    }
+    set z(value) {
+        this._z = value;
+    }
+    sub(vec) {
+        return this.subCoords(vec.x, vec.y, vec.z);
+    }
+    subCoords(dx=0, dy=0, dz=0) {
+        let result = this.clone();
+        result.x -= dx;
+        result.y -= dy;
+        result.z -= dz;
+        return result;
+    }
+    toward(vec, t=1) {
+        let result = this.clone();
+        result.x += t*(vec.x-this.x);
+        result.y += t*(vec.y-this.y);
+        result.z += t*(vec.z-this.z);
+        return result;
+    }
+}
 export {origin, e_x, e_y, e_z};
